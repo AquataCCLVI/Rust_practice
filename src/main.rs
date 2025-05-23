08b78ba1441e9c676189d671a61aa5d186765d28
Aquata <rei0609@hotmail.com>
2025-05-23 16:27:15 +0900

    if文for文の書き方

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,11 @@
 fn main() {
-    let x = 10;
-    let y = 20;
+    let mut sum = 0;
 
-    println!("{}", x + y);
-    println!("{}", x - y);
-    println!("{}", x * y);
-    println!("{}", x / y);
-
-    //型指定しないとi32型として解釈される
-    let fx: f32 = 10.0;
-    let fy: f32 = 20.0;
-
-    println!("{}", fx + fy);
-    println!("{}", fx - fy);
-    println!("{}", fx * fy);
-    println!("{}", fx / fy);
+    for i in 0..100 {
+        if i % 2 == 0 {
+            println!("{}", i);
+        }
+        sum += i;
+    }
+    println!("{}", sum);
 }
