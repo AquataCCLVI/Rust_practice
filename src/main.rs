98b4eccd676ddbd501d086911d6a57d1d97e7bb8
Aquata <rei0609@hotmail.com>
2025-05-23 16:23:43 +0900

    変数宣言・四則演算

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    print!("Hello World!");
+    let x = 10;
+    let y = 20;
+
+    println!("{}", x + y);
+    println!("{}", x - y);
+    println!("{}", x * y);
+    println!("{}", x / y);
+
+    //型指定しないとi32型として解釈される
+    let fx: f32 = 10.0;
+    let fy: f32 = 20.0;
+
+    println!("{}", fx + fy);
+    println!("{}", fx - fy);
+    println!("{}", fx * fy);
+    println!("{}", fx / fy);
 }
