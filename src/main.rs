5781c1164bff658ed8365e1924b4a1f916e77789
Aquata <189510521+AquataCCLVI@users.noreply.github.com>
2025-05-29 15:17:31 +0900

    配列、関数の使い方

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,44 @@
-fn main() {
-    let mut sum = 0;
+//定数定義
+const SIZE: usize = 10;
 
-    for i in 0..100 {
-        if i % 2 == 0 {
-            println!("{}", i);
-        }
-        sum += i;
+//fn 関数名(引数)
+fn init(array: &mut [usize; SIZE]) {
+    for i in 1..=10{
+        array[i - 1] = i;
     }
-    println!("{}", sum);
+}
+
+//戻り値は -> 型で示す 戻り値を指定するときは「;」不要、return 〇〇;のような方法も使用可能
+fn print_array(array: & [usize; SIZE]) -> String{
+    let mut str = String::from("");
+    for i in 0..SIZE{
+        str.push_str(&array[i].to_string());
+        str.push_str(", ");
+    }
+
+    str
+}
+
+fn main() {
+    //[初期値;要素数]
+    let mut a = [0; SIZE];
+
+    for i in 1..=SIZE {
+        a[i - 1] = i;
+    }
+
+    //[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    println!("{:?}", a);
+
+    let b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    for n in b {
+        print!("{}, ", n);
+    }
+
+    println!();
+
+    let mut c = [0; SIZE];
+    init(&mut c);
+    println!("{}", print_array(&c));
 }
