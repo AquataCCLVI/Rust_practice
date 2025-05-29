5527c9ecfe729b03e7e38cf81f4c974ab2745e51
Aquata <189510521+AquataCCLVI@users.noreply.github.com>
2025-05-29 15:33:59 +0900

    クロージャーの定義方法

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,22 @@
 //定数定義
 const SIZE: usize = 10;
 
-//fn 関数名(引数)
-fn init(array: &mut [usize; SIZE]) {
-    for i in 1..=10{
-        array[i - 1] = i;
-    }
-}
-
-//戻り値は -> 型で示す 戻り値を指定するときは「;」不要、return 〇〇;のような方法も使用可能
-fn print_array(array: & [usize; SIZE]) -> String{
-    let mut str = String::from("");
-    for i in 0..SIZE{
-        str.push_str(&array[i].to_string());
-        str.push_str(", ");
-    }
-
-    str
-}
-
 fn main() {
     //[初期値;要素数]
     let mut a = [0; SIZE];
 
-    for i in 1..=SIZE {
+    for i in 1..=10 {
         a[i - 1] = i;
     }
 
-    //[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    println!("{:?}", a);
+    // クロージャー let 名前 = |引数| 定義;
+    let double = |x:& usize| *x * 2;
 
-    let b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-    for n in b {
-        print!("{}, ", n);
+    //iter_mut()は可変可能なイテレータを返す(今回は&mut usizeと解釈される)
+    //C言語のようなポインタと拡張for文の組み合わせ技
+    for n in a.iter_mut(){
+        *n = double(n);
     }
 
-    println!();
-
-    let mut c = [0; SIZE];
-    init(&mut c);
-    println!("{}", print_array(&c));
+    println!("{:?}", a);
 }
